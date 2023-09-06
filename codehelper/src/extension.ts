0d0b72e52dbf404f7b27bc38bdf40b24c9ea5b93
silvialpz <74107990+silvialpz@users.noreply.github.com>
2023-09-06 16:57:58 -0400

    installed openai nodejs library

--- codehelper/src/extension.ts
+++ codehelper/src/extension.ts
@@ -22,5 +22,18 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
+export function chat(context: vscode.ExtensionContext) {
+
+	console.log('Connecting to API');
+
+	let disposable = vscode.commands.registerCommand('codehelper.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from CodeHelper!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
 // This method is called when your extension is deactivated
 export function deactivate() {}
