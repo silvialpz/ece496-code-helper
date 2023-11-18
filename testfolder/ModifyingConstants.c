1431ad705a0d2226c4168aa9939ddbb9f9f315dc
Jenifer Hossain <hossainjenifer@gmail.com>
2023-11-18 14:50:18 -0500

    more test cases

---
+++ testfolder/ModifyingConstants.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+   const int a = 5; 
+   a = 10;
+   return 0;
+}
---
+++ testfolder/PointerErrors.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+   int a = 5; 
+   int *p = 5;
+   return 0;
+}
