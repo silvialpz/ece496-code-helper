1e132ad8e320c785a07b672cd9331d5caf4fd659
Jenifer Hossain <hossainjenifer@gmail.com>
2023-11-18 14:44:05 -0500

    changing test file

---
+++ testfolder/TypeMismatch.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int a = 5 + "string";
+    return 0;
+}
--- testfolder/WrongEnumInitialization.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-enum colors { RED, GREEN, BLUE };
-
-int main(int argc, char** argv) {
-    colors myColor;
-    return 0;
-}
